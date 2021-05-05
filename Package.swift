865645bff90fca6ddf5408c7b7923259f448609c
Alexis <alexis@hola.com>
2021-05-05 10:51:27 -0500

    Bitcode

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibraryXCF",
-            targets: ["MyLibraryXCF", "FW1"]
+            name: "FW1",
+            targets: ["FW1"]
         ),
     ],
     dependencies: [
@@ -22,15 +22,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibraryXCF"
-        ),
         .binaryTarget(name: "FW1",
                       path: "./Sources/MyLibraryXCF/FW1.xcframework"
-        ),
-        .testTarget(
-            name: "MyLibraryXCFTests",
-            dependencies: ["MyLibraryXCF"]
         ),
     ]
 )
--- Sources/MyLibraryXCF/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Alexis Celestino Solís on 04/05/21.
-//
-
-import Foundation
