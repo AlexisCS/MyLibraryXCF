7be91d12ae5d7edbd41dd658d8093855164dfcac
Alexis Celestino S <acelestinos@EKT1662355MACLAP.local>
2021-05-10 18:07:52 -0500

    Fix

--- Package.swift
+++ Package.swift
@@ -20,13 +20,14 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/onevcat/Kingfisher.git", .branch("master"))
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "6.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "MyLibraryXCF",
+            dependencies: ["Kingfisher"],
             exclude: ["FW1.xcframework"],
             resources: [
                 .process("Resources")
